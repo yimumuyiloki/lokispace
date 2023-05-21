b9396a2674e809c1bcffca7ab755d40494b2ca93
yimumuyiloki <yimumuyi.loki@gmail.com>
2023-05-22 00:18:41 +0800

    5.22
    新增了点击效果，更换字体

---
+++ 物理碰撞/index.js
@@ -0,0 +1,10 @@
+// 参考网址：
+// 具体效果：https://brm.io/matter-js/demo/#mixed（物体掉落+点击消失）
+// github：https://github.com/liabru/matter-js（似乎是源代码？）
+
+// 具体要求：同上效果，7个shape，shape里面是图片，鼠标点击之后shape消失，相邻shape之间会有位移。7个shape可能占了半屏？
+// pic width：200px
+// 其他的不需要做
+
+// Ps：在shape的容器里放图片，shape是rectangle，和图片差不多大小就可以了
+// 之前发的工程项目没有太多参考意义。。。
